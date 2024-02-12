3c4d6923be0c43580d7a7c7d980fca08fb3bc0da
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2024-02-12 14:06:53 -0800

    semicolon

--- ethers-v6/src/App.tsx
+++ ethers-v6/src/App.tsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import Capsule, { Button, Environment, OAuthMethod, CapsuleEthersSigner } from '@usecapsule/web-sdk'
-import './App.css'
+import { useState } from 'react';
+import Capsule, { Button, Environment, OAuthMethod, CapsuleEthersSigner } from '@usecapsule/web-sdk';
+import './App.css';
 
-const MESSAGE_TO_SIGN = 'Hello, world!'
+const MESSAGE_TO_SIGN = 'Hello, world!';
 
 function App() {
-  const [isFullyLoggedIn, setIsFullyLoggedIn] = useState(false)
-  const [walletAddress, setWalletAddress] = useState<string | undefined>(undefined)
-  const [messageSignature, setMessageSignature] = useState<string | undefined>(undefined)
-  const capsule = new Capsule(Environment.BETA, 'd0b61c2c8865aaa2fb12886651627271')
+  const [isFullyLoggedIn, setIsFullyLoggedIn] = useState(false);
+  const [walletAddress, setWalletAddress] = useState<string | undefined>(undefined);
+  const [messageSignature, setMessageSignature] = useState<string | undefined>(undefined);
+  const capsule = new Capsule(Environment.BETA, 'd0b61c2c8865aaa2fb12886651627271');
 
   async function signMessage(): Promise<void> {
-    const ethersSigner = new CapsuleEthersSigner(capsule)
-    setMessageSignature(await ethersSigner.signMessage(MESSAGE_TO_SIGN))
+    const ethersSigner = new CapsuleEthersSigner(capsule);
+    setMessageSignature(await ethersSigner.signMessage(MESSAGE_TO_SIGN));
   }
 
   async function checkIfLoggedIn(): Promise<void> {
-    const isloggedIn = await capsule.isFullyLoggedIn()
+    const isloggedIn = await capsule.isFullyLoggedIn();
     if (isloggedIn) {
-      setWalletAddress(Object.values(capsule.getWallets())[0]?.address)
-      setIsFullyLoggedIn(true)
+      setWalletAddress(Object.values(capsule.getWallets())[0]?.address);
+      setIsFullyLoggedIn(true);
     } else {
-      setIsFullyLoggedIn(false)
+      setIsFullyLoggedIn(false);
     }
   }
 
@@ -51,7 +51,7 @@ function App() {
         {messageSignature && <h3>Message Signature for "{MESSAGE_TO_SIGN}": {messageSignature}</h3>}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
