fe466cb22eadfdb8901fa76250dd4797a4768080
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2024-02-12 14:18:27 -0800

    small improvements

--- ethers-v6/src/App.tsx
+++ ethers-v6/src/App.tsx
@@ -2,26 +2,32 @@ import { useState } from 'react';
 import Capsule, { Button, Environment, OAuthMethod, CapsuleEthersSigner } from '@usecapsule/web-sdk';
 import './App.css';
 
+// not sensitive
+const BETA_KEY = 'd0b61c2c8865aaa2fb12886651627271';
 const MESSAGE_TO_SIGN = 'Hello, world!';
 
 function App() {
-  const [isFullyLoggedIn, setIsFullyLoggedIn] = useState(false);
   const [walletAddress, setWalletAddress] = useState<string | undefined>(undefined);
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
   const [messageSignature, setMessageSignature] = useState<string | undefined>(undefined);
-  const capsule = new Capsule(Environment.BETA, 'd0b61c2c8865aaa2fb12886651627271');
+  const capsule = new Capsule(Environment.DEVELOPMENT, BETA_KEY);
 
   async function signMessage(): Promise<void> {
+    if (!(await capsule.isFullyLoggedIn())) {
+      setErrorMessage('User is not logged in');
+      return;
+    }
+    setErrorMessage(undefined);
     const ethersSigner = new CapsuleEthersSigner(capsule);
     setMessageSignature(await ethersSigner.signMessage(MESSAGE_TO_SIGN));
   }
 
   async function checkIfLoggedIn(): Promise<void> {
-    const isloggedIn = await capsule.isFullyLoggedIn();
-    if (isloggedIn) {
+    if (await capsule.isFullyLoggedIn()) {
       setWalletAddress(Object.values(capsule.getWallets())[0]?.address);
-      setIsFullyLoggedIn(true);
+      setErrorMessage(undefined);
     } else {
-      setIsFullyLoggedIn(false);
+      setWalletAddress(undefined);
     }
   }
 
@@ -37,9 +43,9 @@ function App() {
         >
           Check if logged in
         </button>
-        {isFullyLoggedIn ? <h2>User is logged in!</h2> : <h2>User is not logged</h2> }
+        {walletAddress ? <h2>User is logged in!</h2> : <h2>User is not logged in.</h2> }
         <br />
-        {isFullyLoggedIn &&
+        {walletAddress &&
           <button
             onClick={signMessage}
           >Sign Message</button>
@@ -49,6 +55,9 @@ function App() {
         {walletAddress && <h3>Wallet Address: {walletAddress}</h3>}
         <br />
         {messageSignature && <h3>Message Signature for "{MESSAGE_TO_SIGN}": {messageSignature}</h3>}
+        {errorMessage && <h3 style={{
+          color: 'red',
+        }}>Error: {errorMessage}</h3>}
       </div>
     </>
   );
