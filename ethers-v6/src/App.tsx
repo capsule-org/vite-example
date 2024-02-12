b9512dff247c8948ca344e0b59ad5088c54693cc
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2024-02-12 12:39:58 -0800

    button working

---
+++ ethers-v6/src/App.tsx
@@ -0,0 +1,37 @@
+// import { useState } from 'react'
+import Capsule, { Button, Environment, OAuthMethod } from '@usecapsule/web-sdk'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const capsule = new Capsule(Environment.BETA, 'd0b61c2c8865aaa2fb12886651627271')
+  return (
+    <>
+      <Button capsule={capsule} appName="Vite Example" oAuthMethods={[OAuthMethod.GOOGLE]} />
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count} (click here to add 1)
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
---
+++ ethers-v6/src/main.tsx
@@ -0,0 +1,14 @@
+console.log('typeof global', typeof global)
+if (typeof global === 'undefined') {
+  window.global = window;
+}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
